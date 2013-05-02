8f0f2b3eb4cdb960f9a7c1aa3a8ae8815cd2544d
Doug Martin <doug@dougamartin.com>
2013-05-02 01:21:44 -0500

    Updated slides

--- project1/Gruntfile.js
+++ project1/Gruntfile.js
@@ -1,30 +1,5 @@
 /*global module:false*/
 module.exports = function (grunt) {
-    // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-
-        jshint: {
-            file: "./index.js",
-            options: {
-                jshintrc: '.jshintrc'
-            }
-        },
-
-        it: {
-            all: {
-                src: 'test/**/*.test.js',
-                options: {
-                    timeout: 3000, // not fully supported yet
-                    reporter: 'dotmatrix'
-                }
-            }
-        }
-
-    });
-
-    // Default task.
-    grunt.registerTask('default');
     grunt.loadNpmTasks('grunt-link');
 
 };
